477872268c2de0b67dfd76da2ef5338dc2798623
ji-circle <162451391+ji-circle@users.noreply.github.com>
2024-03-29 09:57:14 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/1/131705. 삼총사/삼총사.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(number: IntArray): Int {
+        var answer: Int = 0
+        for(i in 0..number.size-1){
+            for(j in i+1..number.size-1){
+                for(k in j+1..number.size-1){
+                    if(number[i]+number[j]+number[k]==0){
+                        answer+=1
+                    }
+                }
+            }
+        }
+        return answer
+    }
+}
