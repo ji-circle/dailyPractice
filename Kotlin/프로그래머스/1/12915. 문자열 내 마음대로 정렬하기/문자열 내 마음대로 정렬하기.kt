b814ed9e8db2c088f5e18d05827b2b1a0534d80b
ji-circle <162451391+ji-circle@users.noreply.github.com>
2024-04-03 09:25:49 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 23.10 ms, Memory: 64 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.kt
+++ Kotlin/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun solution(strings: Array<String>, n: Int): Array<String> {
         
+        //다시해보기
         
-        return strings.sortedBy{it}.sortedBy{it[n]}
-        .toTypedArray()
+        return strings.sortedBy{it}.sortedBy{it[n]}.toTypedArray()
         
     }
 }
