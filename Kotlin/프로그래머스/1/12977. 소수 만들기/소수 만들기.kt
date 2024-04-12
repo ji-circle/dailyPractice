13e513f7deedbbbeb6d8dae019f193ff202a11aa
ji-circle <162451391+ji-circle@users.noreply.github.com>
2024-04-12 11:10:34 +0900

    [level 1] Title: 소수 만들기, Time: 0.15 ms, Memory: 62.1 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
+++ Kotlin/프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
@@ -8,6 +8,11 @@ class Solution {
             for(j in i+1..nums.size-2){
                 for(k in j+1..nums.size-1){
                     temp = nums[i] + nums[j] + nums[k]
+                    
+                    //if (isPrimeNumber(temp)) {
+                    //    answer++
+                    //}
+                    
                     for(l in 2..temp-1){
                         if(temp%l==0){//소수가 아님
                             isPrime = false
@@ -25,4 +30,7 @@ class Solution {
         }
         return answer
     }
+    //fun isPrimeNumber(num: Int): Boolean {
+    //    return (2..num / 2).filter { num % it == 0 }.count() == 0
+    //}
 }
