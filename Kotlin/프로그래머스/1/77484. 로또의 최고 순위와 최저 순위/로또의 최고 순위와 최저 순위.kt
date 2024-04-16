2929988c3085a232c69c7b3df5d1fdee7e2ab976
ji-circle <162451391+ji-circle@users.noreply.github.com>
2024-04-16 10:02:38 +0900

    Update 로또의 최고 순위와 최저 순위.kt
    주석 추가

--- Kotlin/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
+++ Kotlin/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
@@ -1,14 +1,17 @@
 class Solution {
     fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
+        val win = win_nums.toHashSet()
+        // hashset 사용하면 중복원소 허용 없이 더 빠르게 검색 가능
         var best = 0
         var worst = 0
         lottos.forEach{
             if(it == 0){
                 best++
             }
-            if(win_nums.contains(it)){
-                best++
+            //if(win_nums.contains(it)) 해도 되긴 함
+            if(win.contains(it)){
                 worst++
+                best++
             }
         }
         
