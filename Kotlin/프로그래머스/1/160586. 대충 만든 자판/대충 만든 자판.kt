e32cd5ba9c423c9fb9eb7b6beb9ed93066d946ea
ji-circle <162451391+ji-circle@users.noreply.github.com>
2024-04-23 10:01:08 +0900

    Update 대충 만든 자판.kt

--- Kotlin/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.kt
+++ Kotlin/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.kt
@@ -1,10 +1,11 @@
-//확인
+//다시하기!!
 
 import kotlin.math.*
 
 class Solution {
     fun solution(keymap: Array<String>, targets: Array<String>): IntArray {
         
+        //minPress[X.code - 'A'.code]: X를 입력하기 위해 키를 눌러야하는 횟수의 최솟값
         var minPress = Array(26, {it -> 200})
         for(kmap in keymap){
             for(i in 0 until kmap.length){
@@ -15,9 +16,11 @@ class Solution {
         
         var answer: IntArray = intArrayOf()
         for(t in targets){
+            //문자열 t를 입력하기 위해 키를 눌러야하는 횟수의 최솟값
             var pressSum = 0
             for(i in 0 until t.length){
                 val k = t[i]
+                //k를 입력할 수 없는 경우
                 if(minPress[k.code - 'A'.code] == 200){
                     pressSum = -1
                     break
@@ -29,3 +32,16 @@ class Solution {
         return answer
     }
 }
+
+//class Solution {
+//    fun solution(keymap: Array<String>, targets: Array<String>): IntArray =
+//        targets.map { str ->
+//            str.map { c -> keymap.map { it.indexOf(c) + 1 }
+//                .filterNot { it < 1 }
+//                .let { list ->
+//                    if (list.isEmpty()) -1
+//                    else list.minOf { it }
+//                }
+//            }.let { if ( it.contains(-1)) -1 else it.sum() }
+//        }.toIntArray()
+//}
