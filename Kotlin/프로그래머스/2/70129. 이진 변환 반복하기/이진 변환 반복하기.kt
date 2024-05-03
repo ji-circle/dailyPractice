f6dbc2d26fe743d40d4ebfd9eaacb1e30d16b9a2
ji-circle <162451391+ji-circle@users.noreply.github.com>
2024-05-03 09:48:17 +0900

    Update 이진 변환 반복하기.kt
    예시 추가

--- Kotlin/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.kt
+++ Kotlin/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.kt
@@ -1,14 +1,33 @@
-//
+//다시 해보
 
 class Solution {
     fun solution(s: String): IntArray {
         val answer = IntArray(2)
         var str:String = s
+        // str이 1이 될때까지
         while(str != "1"){
+            // 1회마다 증가
             answer[0]++
+            // 제거된 0의 개수 누적합
             answer[1] += str.replace("1", "").length
+            // str 길이를 2진 변환한 값으로 초기화
             str = str.replace("0", "").length.toString(2)
         }
         return answer
     }
 }
+
+//class Solution {
+//    fun solution(s: String): IntArray {
+//        var copiedS = s
+//        var removedZero = 0
+//        var count = 0
+
+//        while (copiedS != "1") {
+//            removedZero += copiedS.replace("1", "").count()
+//            copiedS = Integer.toBinaryString(copiedS.replace("0", "").count())
+//            count++
+//        }
+//        return intArrayOf(count, removedZero)
+//    }
+//}
