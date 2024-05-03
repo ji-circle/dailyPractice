5a3986393f1d4388271c756f819b30d6b182184e
ji-circle <162451391+ji-circle@users.noreply.github.com>
2024-05-03 09:42:04 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 27.38 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.kt
@@ -0,0 +1,14 @@
+//
+
+class Solution {
+    fun solution(s: String): IntArray {
+        val answer = IntArray(2)
+        var str:String = s
+        while(str != "1"){
+            answer[0]++
+            answer[1] += str.replace("1", "").length
+            str = str.replace("0", "").length.toString(2)
+        }
+        return answer
+    }
+}
