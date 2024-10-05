5a33ad2997e89a187292b438ca35b8085d611991
ji-circle <162451391+ji-circle@users.noreply.github.com>
2024-10-05 19:39:46 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.06 ms, Memory: 60.1 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun solution(my_string: String, is_suffix: String): Int {
+        var answer: Int = 0
+        var stringtemp = my_string.length-1
+        var suffixtemp = is_suffix.length-1
+        var start = stringtemp - suffixtemp
+        if(start < 0){
+            answer = 0
+            return answer
+        }
+        for (j in 0..suffixtemp){
+            if(my_string[start] != is_suffix[j]){
+                answer = 0
+                break
+            }
+            else{
+            start += 1
+            answer = 1       
+        }}
+        return answer
+    }
+}
